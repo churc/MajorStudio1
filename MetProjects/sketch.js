5e0bf779460003bb2b327cee15cffe4f5035e8d7
Clare <ccinflux@hotmail.com>
2018-02-17 02:22:15 -0500

    Add files via upload

--- MetProjects/sketch.js
+++ MetProjects/sketch.js
@@ -1,6 +1,24 @@
 var table;
 
+var yrFemTotals = [];
+
+var barTotals =[];
+
+var FemaleTotal = [];
+
+var TotalFemale = [];
+
+var TotalMale = [];
+
+var TotalTwo = [];
+
+var TotalUnknown = [];
+
+var TotalToFindOut = [];
+
 var gender = [];
+
+var artist = [];
 
 var ArtistColl = [];
 
@@ -8,7 +26,7 @@ var allYears = [];
 
 var whatGender = [];
 
-var minGender, maxGender, minYear, maxYear, minObjects, maxObjects, maxArtistColl, artist, gender, gen;
+var minGender, maxGender, minYear, maxYear, minObjects, maxObjects, maxArtist, maxArtistColl;
 
 var margin = 50;
 
@@ -24,6 +42,7 @@ function setup(){
 	 analyzeData();
 	 displayData();
 	 drawLabels();
+   analyzeGender();
 }
 
 function analyzeData(){
@@ -32,198 +51,49 @@ function analyzeData(){
 	console.log(count + " rows");
   console.log(countC + " columns");
 
-// ///=========THIS shows row with FIRST female artist
-///using gender column 10
+  
 
- // 	var female = table.findRow('f', 10);
- // 	console.log(female);
+// ///THIS shows row with FIRST female artist gender column 10
+ 	// var female = table.findRow('f', 10);
+ 	// console.log(female);
+////============
 
-// //==========
+//=======THIS console logs all rows with a FEMALE artist
+    var female = table.findRows('f', 10);
+    console.log(female);
 
-//=======THIS console logs all rows with a female artist
+//=======THIS console logs all rows with a MALE artist
+    var male = table.findRows('m', 10);
+    console.log(male);
 
-////GET NUMBER COUNT AS BAR CHART
+//=======THIS console logs all rows where BOTH male and female named as artists 
+    var couple = table.findRows('t', 10);
+    console.log(couple);
 
-   var female = table.findRows('f', 10);
-   console.log(female);
-
-//==========
-
-//=======THIS console logs all rows with a male artist
-
-   var male = table.findRows('m', 10);
-   console.log(male);
-
-// //==========
-
-//=======THIS console logs all rows where both male and female named as artists 
-
-     var couple = table.findRows('t', 10);
-//    console.log(unknown + " artists in ModCont collection where artist is unknown");
-      console.log(couple);
-
-//=========
-
-//=======THIS console logs all rows where the artist is unknown 
-
-     var unknown = table.findRows('u', 10);
-//    console.log(unknown + " artists in ModCont collection where artist is unknown");
-      console.log(unknown);
-
-// //==========
-
+//=======THIS console logs all rows where the artist is UNKNOWN
+    var unknown = table.findRows('u', 10);
+    console.log(unknown);
 
 //=======THIS console logs all rows where there is an artist but I haven't found their gender
-
    var todo = table.findRows('a', 10);
    console.log(todo);
-// };
 // // //===================================
 
 
-// ///=========try to do count by years 
-//   var yearNow = {};
-//   yearNow.year = (int(table.getString(0,17)));
-//   yearNow.items = table.findRows(String(yearNow.year),17);
-//   append(allYears, yearNow);
-
-//   minGender = 150000;
-//   maxGender = 0;
-//   minObjects = 150000;
-//   maxObjects = 0;
-//   minYear = null;
-//   maxYear = null;
-
-
-// for (var i=0; i<count; i++) {
-//       var year = int(table.getString(i,17));
-//       if(year!=yearNow.year){
-//         var yearNow = {};
-//         yearNow.year = year;
-//         yearNow.items =[];
-//         yearNow.items = table.findRows(String(yearNow.year),17);
-//         append(allYears, yearNow);
-        
-//         if(yearNow.items.length>maxObjects){
-//           maxObjects = yearNow.items.length;
-//           maxYear = allYears.length-1;
-//         }
-//         if(yearNow.items.length<minObjects){
-//           minObjects = yearNow.items.length;
-//           minYear = allYears.length-1;
-//         }
-//      }
-//   }
-//   console.log("The Year " + allYears[maxYear].year + " has the most objects with " + allYears[maxYear].items.length + " items.");
-
-// };
-
-
-///=========try to do for number of female artists  
-//   var gender = {};
-
-//   gender.gen = (String(table.getString(i,10)));
-
-//   gender.items = table.findRows(String(gender.gen),10);
-//   append(whatGender, gender);
-
-//   console.log(gender);
-
-//   maxwhatGender = 120000;
-//   maxArtistColl = 3000;
-//   minGender = 150000;
-//   maxGender = 0;
-//   minObjects = 150000;
-//   maxObjects = 0;
-//   minYear = null;
-//   maxYear = null;
-
-
-// for (var i=0; i<count; i++) {
-//       //var artist = int(table.getString(i,11));
-
-//       var gen = table.getString(i,10);
-//       if(gen!=gender.gen){
-//         var gender = {};
-//         gender.gen = gen;
-//         gender.items =[];
-//         gender.items = table.findRows(String(gender.gen),10);
-//         append(whatGender, gender);
-
-//         console.log(whatGender);
-        
-//         // if(gender.items.length>maxwhatGender){
-//         //   maxwhatGender = gender.items.length;
-//         //   maxGender = whatGender.length-1;
-//         // }
-       
-//      }
-//   }
-//   // console.log( "The " + (whatGender[maxwhatGender].gen) + "gender is most represented in the ModCont collection with " + whatGender[maxwhatGender].items.length + " items by artists of that gender" );
-// };
-///========
-
-
-// ///=========THIS WORKS to get max number of items by one artist 
-/// using display name column 11 
-// 	var displayName = {};
-
-//   displayName.artist = (String(table.getString(0,11)));
-
-// 	displayName.items = table.findRows(String(displayName.artist),11);
-// 	append(ArtistColl, displayName);
-
-//   console.log(displayName);
-
-
-//   maxArtistColl = 3000;
-//   minGender = 150000;
-//   maxGender = 0;
-//   minObjects = 150000;
-//   maxObjects = 0;
-//   minYear = null;
-//   maxYear = null;
-
-
-// for (var i=0; i<count; i++) {
-//       //var artist = int(table.getString(i,11));
-
-//       var artist = table.getString(i,11);
-//       if(artist!=displayName.artist){
-//         var displayName = {};
-//         displayName.artist = artist;
-//         displayName.items =[];
-//         displayName.items = table.findRows(String(displayName.artist),11);
-//         append(ArtistColl, displayName);
-        
-//         if(displayName.items.length>maxArtistColl){
-//           maxArtistColl = displayName.items.length;
-//           maxArtist = ArtistColl.length-1;
-//         }
-       
-//      }
-//   }
-//   console.log("The artist" + ArtistColl[maxArtistColl].artist + " that has the most objects has " + ArtistColl[maxArtistColl].items.length + " items by " + (String(maxArtistColl.displayName)));
-// };
-
-//============================
-
-// ///=========THIS works to get max number of years
-///using object bein date 17 
+// ///=========WORKS to give the YEARS w/ the MAX & MIN NUMBER OF OBJECTS 
+////object begin date column 17
   var yearNow = {};
   yearNow.year = (int(table.getString(0,17)));
+
   yearNow.items = table.findRows(String(yearNow.year),17);
   append(allYears, yearNow);
 
-  // minGender = 150000;
-  // maxGender = 0;
-  minObjects = 100000;
+  minObjects = 150000;
   maxObjects = 0;
   minYear = null;
   maxYear = null;
 
-
-for (var i=0; i<count; i++) {
+ for(var i=0; i<count; i++){
       var year = int(table.getString(i,17));
       if(year!=yearNow.year){
         var yearNow = {};
@@ -236,93 +106,228 @@ for (var i=0; i<count; i++) {
           maxObjects = yearNow.items.length;
           maxYear = allYears.length-1;
         }
-
         if(yearNow.items.length<minObjects){
           minObjects = yearNow.items.length;
-          minYear = allYears.length-1;
+          minYear = allYears.length;
         }
      }
   }
- //  console.log("The Year " + allYears[maxYear].year + " has the most objects with " + allYears[maxYear].items.length + " items.");
- // console.log("The Year " + allYears[minYear].year + " has the least objects with " + allYears[minYear].items.length + " items.");
+  console.log("The Year " + allYears[maxYear].year + " has the most objects with " + allYears[maxYear].items.length + " items.");
+  console.log("The Year " + allYears[minYear].year + " has the least objects with " + allYears[minYear].items.length + " item.");
 }
 
 
-//////====================
 
- ///set a margin and display data
+/////=================
 
+
+///======THIS WORKS FOR TOTAL NUMBER of FEMALE, MALE, TWO, UNKNOWN artists  
+
+function analyzeGender(){
+  var cnt = table.getRowCount();
+  var gender = {};
+
+  var MaleArtists = 0;
+  var FemaleArtists = 0;
+  var TwoArtists = 0;
+  var UnknownArtists = 0;
+  var ToFindOutArtists = 0;
+  var Other = 0;
+
+  gender.gen = (String(table.getString(0,10)));
+
+  for(var g=0; g<cnt; g++){
+    var gender = table.getString(g,10);
+
+    gender.items = table.findRows(String(gender.gen),10);
+  if (gender === 'm') {
+      MaleArtists = MaleArtists + 1;
+
+    } else if (gender === 'f') {
+     FemaleArtists = FemaleArtists + 1;
+
+     } else if (gender === 't') {
+     TwoArtists = TwoArtists + 1;
+
+    } else if (gender === 'u') {
+     UnknownArtists = UnknownArtists + 1;
+
+   } else if (gender === 'a') {
+     ToFindOutArtists = ToFindOutArtists + 1;
+
+   } else {
+     Other = Other + 1;
+     }
+
+  }
+
+    TotalFemale.push({femaleTotal: FemaleArtists});
+    console.log(TotalFemale);
+
+    TotalMale.push({maleTotal: MaleArtists});
+    console.log(TotalMale);
+
+    TotalTwo.push({twoTotal: TwoArtists});
+    console.log(TotalTwo);
+
+    TotalUnknown.push({unknownTotal: UnknownArtists});
+    console.log(TotalUnknown);
+
+    TotalToFindOut.push({tofindoutTotal: ToFindOutArtists});
+    console.log(TotalToFindOut);
+       
+
+}
+///========
+
+
+// ///=========max number of items by one artist 
+/// using display name column 10 
+	
+//   var rows = table.getRowCount();
+
+//   var displayName = {};
+//   displayName.artist = table.getString(0,9);
+//   displayName.items = table.findRows(String(displayName.artist),9);
+// 	append(ArtistColl, displayName);
+ 
+//   maxArtistColl = 0;
+//   maxArtist = null;
+    
+
+
+// for (var n=0; n<rows; n++) {
+//       //var artist = int(table.getString(i,11));
+
+//       var artist = (String(table.getString(n,9)));
+//       if(artist!=displayName.artist){
+//         var displayName = {};
+//         displayName.artist = artist;
+//         displayName.items =[];
+//         displayName.items = table.findRows(String(displayName.artist),10);
+//         append(ArtistColl, displayName);
+//         // console.log(ArtistColl);
+//         // console.log(ArtistColl)
+//         if(displayName.items.length>maxArtistColl){
+//           maxArtistColl = displayName.items.length;
+//           maxArtist = ArtistColl.length-1;
+//           console.log(maxArtist);
+//           console.log(ArtistColl[maxArtist].items.length)
+//           //maxArtistName = ArtistColl.length.ArtistColl;
+//         }
+//      }
+// //      // console.log("thisis maxArtist" + maxArtist);
+// //      // console.log("thisis ArtistColl length" + ArtistColl.length);
+// //       // console.log("The artist" + ArtistColl[maxArtistColl].artist + " that has the most objects has " + ArtistColl[maxArtistColl].items.length + " items by " + (String(maxArtistColl.displayName)));
+//   }
+// console.log("The maximum number of artworks by one artist " + ArtistColl[maxArtist].artist + "by" + ArtistColl[maxArtist].items.length);
+
+// // //   console.log("The artist" + ArtistColl[maxArtistColl].artist + " that has the most objects has " + ArtistColl[maxArtistColl].items.length + " items by " + (String(maxArtistColl.displayName)));
+// // // };
+// }
+
+//============================
+
+
+//WORKS TO DISPLAY PER YEAR by GENDER
 function displayData(){
 	noStroke();
 	fill(255,0,0);
 
-
 	for(var i=0; i<allYears.length; i++){
-		//console.log(i)
-  //// years use map incoming value and range
-    // allYears[allYears.length-1].year;
-    //map function 
+//// years use map incoming value and range
+    // console.log('thisisallYrs' + allYears[allYears.length-1].year);
+////map function 
     var x = map(allYears[i].year,1800,allYears[allYears.length-1].year, margin, width-margin);
 		
-    // var x = map(allYears[i].year,1430,allYears[allYears.length-1].year, margin, width-margin);
-    //write in console log where it is mapping that is relatively corresponding to
-    console.log("mapping: " + allYears[i].year + "to: " + x);
+    var totalFemaleArtistsPerBar = 0;
+
+////THIS logs where it is mapping that is relatively corresponding to:
+   // console.log("mapping: " + allYears[i].year + "to: " + x);
+
     // var x = map(i,0,allYears.length, margin, width-margin);
 		//rect(x,height/2,5,5);
+
   ///then do same for y height
 	for(var j=0; j<allYears[i].items.length; j++){
 		  var y= map(j,0,maxObjects,height-margin, margin);
-      ///FILL BY GENDER
-		  // fill(random(255),random(255),random(255))
-      //fill('rgba(100%,0%,100%,0.5)');
 
+//table.findRows(String(yearNow.year),17)
+
+///====WORKS TO FILL BY GENDER
+   var gender = allYears[i].items[j].obj.Gender;
     if (gender === 'm') {
       fill(154,205,50);
+      rect(x,y,2,1);
+       // console.log('m');
+
     } else if (gender === 'f') {
       fill(186,85,211);
+       rect(x,y,2,1);
+       // console.log('f');
+     totalFemaleArtistsPerBar = totalFemaleArtistsPerBar + 1;
+     // console.log(totalFemaleArtistsPerBar);
     } else if (gender === 't') {
-      fill(154,205,50);
+      fill(204,147,34);
+       rect(x,y,2,1);
+      // console.log('t');
+      
     } else if (gender === 'u') {
-      fill(154,205,50);
+      fill(169,169,169);
+       rect(x,y,2,1);
+       // console.log('u');
+
     } else if (gender === 'a') {
-      fill(154,205,50);
+      fill(0,0,0);
+       rect(x,y,2,1);
+       // console.log('a');
+
     } else {
-      fill(154,205,50);
-    };
+      fill(169,169,169);
+       rect(x,y,2,1);
+      // console.log('u');
+    }
 
-
-
-
-
-		  rect(x,y,1,1);
-		//ellipse(x,y,2,2);
 	}
-  }
 
+ 
+//THIS gets number of FEMALE artworks per BAR with total bar height
+  barTotals.push({barTotal: allYears[i].items, femaleTotal: totalFemaleArtistsPerBar});
+
+//THIS gets number of FEMALE artworks per YEAR
+  yrFemTotals.push({year: allYears[i].year, total: totalFemaleArtistsPerBar});
+
+  }
 }
+
+
 
 
 function drawLabels(){
 
 	//x axis
-	stroke(0);
+	stroke(77,77,77);
 	//just the lines
  	line(margin,height-margin,width-margin,height-margin);
   	noStroke();
   	textAlign(CENTER);
 
   // draw the sections and add text for each section
-		//go throught the years
-		for(var i=0; i<allYears.length; i+=5){
-    var y = height-margin+30;
-  x = map(allYears[i].year,1800,allYears[allYears.length-1].year, margin, width-margin);
+	//go throught the years from 1800-2017
+  for(var i=1800; i<=2017; i+=10){
+   var y = height-margin+30;
+    x = map(i,1800,2017, margin, width-margin);
     // x = map(i,0, allYears.length,margin, width-margin);
     noStroke();
-    fill(0);
-    text(allYears[i].year, x, y);
-    stroke(0);
-    line(x,y-12,x, y-30);
-  }
+    fill(77,77,77);
+    // fill(0);
+    text(i, x, y);
+    stroke(77,77,77);
+    strokeWeight(1);
+    // stroke(0);
+    line(x,y-22,x, y-30);
+    //line(x,y-12,x, y-30);
+}
 
   // label the whole axis
   textAlign(RIGHT);
@@ -331,7 +336,7 @@ function drawLabels(){
   text("Year", width-margin,height-margin+70);
 
 //   // 2. Let's draw the y Axis
-  stroke(0);
+  stroke(77,77,77);
   line(margin,height-margin ,margin,margin);
   noStroke();
   textAlign(RIGHT);
@@ -341,10 +346,14 @@ function drawLabels(){
     var x = margin-20;
     y = map(i,0, maxObjects,height-margin, margin);
     noStroke();
-    fill(0);
+    fill(77,77,77);
+    //fill(0);
     text(i, x, y+5);
-    stroke(0);
-    line(x+5,y,x+20,y);
+    stroke(77,77,77);
+    strokeWeight(1);
+    // stroke(0);
+    line(x+10,y,x+20,y);
+    // line(x+5,y,x+20,y);
   }
   textStyle(BOLD);
   noStroke();
@@ -354,14 +363,15 @@ function drawLabels(){
   textStyle(BOLD);
   noStroke();
   textAlign(LEFT);
-  textSize(20);
-  //textColor("#o2o5o5");
+  textSize(15);
+  fill(160,42,85);
   text("Gender of Artists Represented in the Met Modern & Contemporary Art Collection", margin,margin-30);
   textSize(12);
   textStyle(NORMAL);
   text("1800 - 2017", margin,margin-15);
 }
 
+/////============================================
 
 
 
@@ -369,23 +379,12 @@ function drawLabels(){
 
 
 
-
-
-//============
-	// var row = table.getRowCount();
-	// var column = table.getColumnCount();
-
-//=========
-//works to read the table object in console log
-// var tableObject = table.getObject();
-// console.log(tableObject);
 //=========
 //to get all the rows
 //warning: rows is an array of objects
 // 	for (var i=0; i<row.length; i++) {
 //   		rows[i].set('name', 'MetFullObjects');
-  	
-//   		
+   		
 // //show the results
 // 		var rows = table.getRows();
 // 		var span = createSpan(rows(i) + " | ");
