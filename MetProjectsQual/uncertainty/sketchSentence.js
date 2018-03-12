a95a0c7b082274b870a90c7f151a318c94835aaf
Clare <ccinflux@hotmail.com>
2018-03-12 09:40:49 -0400

    Update sketchSentence.js

--- MetProjectsQual/uncertainty/sketchSentence.js
+++ MetProjectsQual/uncertainty/sketchSentence.js
@@ -87,28 +87,28 @@ $(document).ready(function(){
 
 
 
-$('p').each(function() {
-    				var sentences = $(this).text().replace(/([^.!?]*[^.!?\s][.!?]['"]?)(\s|$)/g, 
-                		 '<span class="sentence">$1</span>$2');
-    				$(this).html(sentences);
-				console.log(sentences):
+// $('p').each(function() {
+//     				var sentences = $(this).text().replace(/([^.!?]*[^.!?\s][.!?]['"]?)(\s|$)/g, 
+//                 		 '<span class="sentence">$1</span>$2');
+//     				$(this).html(sentences);
+// 				console.log(sentences):
 				
-				$('.sentence').hover(function(){ 
-					if(!items[n].message && items[n].webLabel.text.includes("uncertain")){
+// 				$('.sentence').hover(function(){ 
+// 					if(!items[n].message && items[n].webLabel.text.includes("uncertain")){
 					
-					var selDes = items[n];
-					var textD = selDes.webLabel.text;
-					if (textD){
-						///find sentence with uncertain in it
-						console.log(textD($(this).text()));
+// 					var selDes = items[n];
+// 					var textD = selDes.webLabel.text;
+// 					if (textD){
+// 						///find sentence with uncertain in it
+// 						console.log(textD($(this).text()));
 						
-					}
-					/////
-					var textD = "Label: " + selDes.webLabel.text;
-				var p = createP(textD)
-				p.addClass("uncertain-des");
+// 					}
+// 					/////
+// 					var textD = "Label: " + selDes.webLabel.text;
+// 				var p = createP(textD)
+// 				p.addClass("uncertain-des");
 
-				p.id('des'+n);
-				p.parent(wrapper);	
-						console.log(selDes); 
+// 				p.id('des'+n);
+// 				p.parent(wrapper);	
+// 						console.log(selDes); 
 	
