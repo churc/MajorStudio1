aa22fe078d59f5a1f7903d84752d73017dfe9976
Clare <ccinflux@hotmail.com>
2018-03-30 15:41:39 -0400

    Add files via upload

--- MetProjectsQual/uncertainty/sketchUncertainPub4.js
+++ MetProjectsQual/uncertainty/sketchUncertainPub4.js
@@ -7,6 +7,7 @@ var uncertain;
 var sentence;
 var sent = [];
 var d;
+var MetArray = [];
 
 function preload() {
     items = loadJSON("assets/UncertainPub.json", showImg);
@@ -14,14 +15,11 @@ function preload() {
 
 
 function setup() {
-  showImg();
   noCanvas();
-  // var cnv = createCanvas(windowWidth, windowHeight);
-  // cnv.style('display', 'block');
   show();
   mouseClicked('span');
   noLoop(); 
-  rita();
+  // rita();
 }
 
 // function windowResized() {
@@ -33,34 +31,16 @@ function show() {
     $('p#title').addClass('titleW');
     $('p#date').addClass('titleW');
     $('p#credit').addClass('titleW'); 
-    // $('p#title').removeClass('titleW');
   })
-//     else
 }
 
 function mouseClicked(){
   show = !show;
-  // if('p#title' ==="#393636"){
-  //   color="#ffffff";
-  // }
 }
-
-function rita () {
-	function loadRandomImage(){
-	var num = met_object.length;
-	
-	array = RiTa.randomOrdering(num);
-	return array;
-	}
-};
-
-
 	
 
 function showImg() {
-	// console.log(items[413]);
-	// console.log(items[2]);
-	// console.log(items[0]);
+
 	var length = Object.keys(items).length-1;
 	// console.log(length);
 
@@ -69,7 +49,7 @@ function showImg() {
  			var met_object = document.createElement('div');
  			met_object.id = "met_object"+n;
 
- 			met_object.addClass="designed-image";
+ 			$(met_object).addClass("designed-image");
  			// console.log(met_object);
 ////image
  			if (!items[n].message) {
@@ -85,8 +65,7 @@ function showImg() {
 	 				obj_url.appendChild(unc);
 
 	 				met_object.appendChild(obj_url);
-
-	 		}
+	 		};
 
 ////title
 			if (!items[n].message && items[n].titles.primaryTitle.includes("uncertain")) {
@@ -103,11 +82,10 @@ function showImg() {
 					// title.id = 'title'+n;
 					met_object.appendChild(title);
 					// console.log(title);
-		};
+			};
 
- 	
 ////tombstone date
-	if (!items[n].message && items[n].tombstone["1"].text.includes("uncertain")) {
+			if (!items[n].message && items[n].tombstone["1"].text.includes("uncertain")) {
 					var date = document.createElement('p');
 	
 					var selDate = items[n];
@@ -124,7 +102,7 @@ function showImg() {
 			};
 
 ////tombstone date is in this array for some objects
-	if (!items[n].message && items[n].tombstone["0"].text.includes("uncertain")) {
+			if (!items[n].message && items[n].tombstone["0"].text.includes("uncertain")) {
 					var date = document.createElement('p');
 					
 					var selDate = items[n];
@@ -141,7 +119,7 @@ function showImg() {
 			};
 
 ////credit
-	if (!items[n].message && items[n].tombstone<=["6"]  && items[n].tombstone["6"].label.includes("Uncertain")) {
+			if (!items[n].message && items[n].tombstone<=["6"]  && items[n].tombstone["6"].label.includes("Uncertain")) {
 				
 					var credit = document.createElement('p');
 
@@ -157,9 +135,7 @@ function showImg() {
 
 					met_object.appendChild(credit);
 					console.log(credit);
-
 			};
-
 
 
 ////description label keep hidden - direct to Met website for web label text
@@ -170,22 +146,18 @@ function showImg() {
 					// var lengthSelD = Object.keys(selDesc).length-1;
 					var textD = selDesc.webLabel.text;
 
-							if(!items[n].message){
+						if(!items[n].message){
 								var de = textD;
-								var rs = new RiString(de);
-							
 						////split text into separate sentences
-							var sent = RiTa.splitSentences(rs.toString());
+							var sent = de.split('.');
 							// console.log(sent);
-							// console.log(sent.length);
-						// /////go through sentences and return each of those with uncertain
+						///////go through sentences and return each of those with uncertain
 							for (var j=0; j<sent.length; j++){
-								if (sent[j].includes("uncertain")){
+								if (sent[j].includes("uncertain"||"Uncertain")){
 									var d = sent[j];
-									// console.log(d);
-									}
-								}			
-							// }
+									d = d.replace(/[<]br[^>]*[>]/gi,"");
+								 }			
+							}
 								desc.innerText += d;
 								desc.addClass = "uncertain-des";
 								desc.id = 'des';
@@ -194,79 +166,33 @@ function showImg() {
 								// console.log(desc);
 						};
 					}
-	 					document.body.appendChild(met_object);
- 						console.log(met_object);
+	 				document.body.appendChild(met_object);
+ 					// console.log(met_object);
+ 					MetArray.push(met_object);	
 
 		};
 
+		// console.log(MetArray);
+
 ////span to isolate word uncertain - for styling	
-				$('p').each(function(){
+			$('p').each(function(){
     		var uncert = $(this).text().replace(/uncertain/g,"<span>uncertain</span>");
     		$(this).html(uncert);
     		// console.log(uncert);
 			});
-
-
  };
 
-
-// $(function () {
-// 		var parent = $("#div");
-
-// 		var div = parent.children();
-// 		// console.log(div);
-// 		// console.log(divs.length);
-// 		while (div.length) {
-// 		        // document.body.appendChild(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-// 		        parent.append(div.splice(Math.floor(Math.random() * div.length), 1)[0]);
-// 		}
-// 	});
-
-////note don't show desc on visualization				
-			// if(!items[n].message && items[n].webLabel.text.includes("uncertain")){
-			
-			// 		var desc = document.createElement('p');
-
-			// 		var selDesc = items[n];
-			// 		// var lengthSelD = Object.keys(selDesc).length-1;		
-			// 		var textD = "Label: " + selDesc.webLabel.text;
-			// 		// console.log(textD);
-			// 		desc.innerText += textD;
-			// 		desc.addClass = "uncertain-des";
-			// 		desc.id = 'des'+n;
-			// 		met_object.appendChild(desc);
-			// 		console.log(desc);
-//////////////
-
-
-
-
-
-			/////not working
-				// $('p').each(function() {
-    // 				var sentences = $(this).text().replace(/([^.!?]*[^.!?\s][.!?]['"]?)(\s|$)/g, 
-    //             		 '<span class="sentence">$1</span>$2');
-    // 				$(this).html(sentences);
-				// console.log(sentences):
-				
-				// $('.sentence').on(function(){ 
-				// 	if(!items[n].message && items[n].webLabel.text.includes("uncertain")){
-					
-				// 	var selDes = items[n];
-				// 	var textD = selDes.webLabel.text;
-				// 	if (textD){
-				// 		///find sentence with uncertain in it
-				// 		console.log(textD($(this).text()));
-						
-				// 	}
-					/////
-					// var textD = "Label: " + selDes.webLabel.text;
-				// var p = createP(textD)
-				// p.addClass("uncertain-des");
-
-				// p.id('des'+n);
-				// p.parent(wrapper);	
-						// console.log(selDes); 
+function shuffleMyArray(){
+		$('.designed-image').remove()
+		shuffle(MetArray,true);
+		for (var r=0; r<MetArray.length; r++){
+			document.body.appendChild(MetArray[r]);
+		}
+		$('p').each(function(){
+    		var uncert = $(this).text().replace(/uncertain/g,"<span>uncertain</span>");
+    		$(this).html(uncert);
+			});
+}
 
 
 
@@ -276,74 +202,7 @@ function showImg() {
 
 
 
-	
-// Object.keys(n).forEach(function(key, index){
- 			// 	items[n][key].includes("uncertain")
- 			// })
-/////WORKS image on browser
-
-// ////WORKS TO load & show JSON images on browser
-// var items = [];
-// var imag
-// var KhandFont, fontReady = false;
 
 
-// function fontRead(){
-//       fontReady = true; 
-//     }
-
-// function preload() {
-//   //my table is comma separated value "csv"
-//   //and has a header specifying the columns labels
-//   // items = loadTable("assets/testapi.JSON", data);
-//     items = loadJSON("assets/testapi.JSON", JSON);
-//     KhandFont = loadFont('libraries/Khand-Regular.ttf', fontRead);
-// }
 
 
-// function setup() {
-//   createCanvas(20,20);
-//   showImg();
-//   textFont('Khand');
-// 	// var imag = document.createElement("img");
-// 	// createImg('https://images.metmuseum.org/CRDImages/ad/web-large/194082.jpg');
-// 	// createImg(items[7].media.images.primaryImage.webImageUrl);
-// 	// items[7].media.images.primaryImage.webImageUrl
-
-//   // randomPlace();
-//   // transparent();
-//   console.log("hi"); 
-// }
-
-// function showImg() {
-	
-// 	var length = Object.keys(items).length-1;
-
-// 		for(var n=0; n<length; n++){
- 			
-//  			  // var imag = document.createElement("img");
-
-//  			  	if(!items[n].message){
-//  				// imag.src = items[n].media.images.primaryImage.imageUrl;
-//  				var img = createImg(items[n].media.images.primaryImage.imageUrl);
-//  				img.addClass("designed-image");
-//  				// imag.src.push(items[n].media.images.primaryImage.imageUrl);
- 		
- 			
-//  			// imag.id = 'unc' + n;
- 			
-
-//  			// document.body.appendChild(imag);	
-//  			// console.log(items[n].media.images.primaryImage.webImageUrl);
-//  			console.log(n);
-//  			}
-//  			// console.log(items[n]);
-
-// 	}		
- 			
-// 		console.log(items[7].media.images.primaryImage.webImageUrl);
-// 		// console.log(items[n].media.images.primaryImage.webImageUrl);
-// 		// console.log(items[n]);
-		
-		
-// 	};
