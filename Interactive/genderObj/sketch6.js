518783dcc4248ace524ec02c071ab2e4ff89cab2
Clare <ccinflux@hotmail.com>
2018-04-13 16:57:21 -0400

    Add files via upload

--- Interactive/genderObj/sketch6.js
+++ Interactive/genderObj/sketch6.js
@@ -32,7 +32,10 @@ var minDate, maxDate, name, gender, date, type;
 
 var margin = 80;
 
+
+
 function collate(array, prop){
+   // p.collate(array, prop) = function{
     return array.reduce(function(groups, item){
     const val = item[prop]
     groups[val] = groups[val] || []
@@ -41,39 +44,46 @@ function collate(array, prop){
   }, {})
 }
 
-   function preload(){
-   // table = loadTable('assets/ModContGenderfinalgosort.csv','csv','header');  /////secondary sort gender, classification
-   table = loadTable('assets/ModContGenderfinalgnoysort.csv','csv','header'); /////four sort gender, Artist Alpha Sort, classification, year
-   console.log(table);
+  //  function preload(){
+  //  // table = loadTable('assets/ModContGenderfinalgosort.csv','csv','header');  /////secondary sort gender, classification
+  //  table = loadTable('assets/ModContGenderfinalgnoysort.csv','csv','header'); /////four sort gender, Artist Alpha Sort, classification, year
+  //  console.log(table);
 
+  // }
+
+var s = function(p){
+   p.preload = function(){
+   // table = loadTable('assets/ModContGenderfinalgosort.csv','csv','header');  /////secondary sort gender, classification
+   p.table = p.loadTable('assets/ModContGenderfinalgnoysort.csv','csv','header'); /////four sort gender, Artist Alpha Sort, classification, year
+   console.log(p.table);
   }
 
-    function setup(){
-    canvas = createCanvas(1200, 800);
+    p.setup = function(){
+    canvas = p.createCanvas(p.windowWidth, p.windowHeight);
     canvas.parent("#c2");
-    analyzeData();
-    categorize();
-    lineChart();
-    handleDataLoad();
-    // barChart();
-    noLoop();
+    p.analyzeData();
+    // p.categorize();
+    // p.lineChart();
+    // handleDataLoad();
+    p.barChart();
+    p.noLoop();
   }
 
 
 ////group by type classification & gender, then by name
 
-  function analyzeData(){
-    gender = table.getColumn(10);  ////gender
-    name = table.getColumn(12); ////name, Artist Alpha Sort
-    date = table.getColumn(17); ////object begin date
-    type = table.getColumn(22); ////classification
+    p.analyzeData = function(){
+    gender = p.table.getColumn(10);  ////gender
+    name = p.table.getColumn(12); ////name, Artist Alpha Sort
+    date = p.table.getColumn(17); ////object begin date
+    type = p.table.getColumn(22); ////classification
   
-    tableA = table.getArray(); 
+    tableA = p.table.getArray(); 
    
-    var female = table.findRows('f', 10); /////2027 rows with a FEMALE artist 
+    var female = p.table.findRows('f', 10); /////2027 rows with a FEMALE artist 
     // console.log(female);
 
-    var male = table.findRows('m', 10);  /////10829 rows with a MALE artist
+    var male = p.table.findRows('m', 10);  /////10829 rows with a MALE artist
     // console.log(male);
 
 //=======  group sort by artist by object type
@@ -108,74 +118,64 @@ function collate(array, prop){
     // console.log(tableAkeys); //returns array of object keys (glass, sculpture etc)
 
 ////iterate through object by keys and SPLITS object into 105 objects 
-////TRY ordering by largest array to smallest, selecting if gender is female
-/////This separates each - need slice.sort() to order??
     var tableAkeys1;
     for(var s=0; s<tableAkeys.length; s++){
-      sort[tableAkeys[s]] = collate(groupedByType[tableAkeys[s]], 22)
-////DOESN't DO LARGEST TO SMALLEST
-      console.log(sort[tableAkeys[s]]);
-      console.log(tableAkeys[s]);
-      // tableAkeys1.slice().sort()=sort[tableAkeys[s]];
+      p.sort[tableAkeys[s]] = collate(groupedByType[tableAkeys[s]], 22)
+      // console.log(tableAkeys[s]);
     }
-    ////NOT WORKING
-      // tableAkeys1.slice().sort()=sort[tableAkeys[s]];
-      // console.log(tableAkeys1[s]);
-
-      // if(tableAkeys[s].includes("f")){    ////selecting by gender not working
-      //   return femaleObj
-
-      // }
-      // console.log(femaleObj); ///105 separate objects
-    // }
+      console.log(tableAkeys); ///105 separate objects 
 }
 
 ////NOT working, list of classifications - map object holds key value pairs
-function categorize(){
-  var groupedByTypeA;
-  var category;
-  var groupedByType = collate(tableA,22);
-  console.log(groupedByType);
+//   p.categorize = function(){
+//   var groupedByTypeA;
+//   var category;
+//   var groupedByType = collate(tableA,22);
+//   console.log(groupedByType);
   
-  groupedByTypeA = Object.keys(groupedByType);
-  console.log(groupedByTypeA);
-    for (var r = 0; r<groupedByTypeA.length; r++) {
-        var category = groupedByTypeA[r];
-        // console.log(category);  ////each of the 105 objects separate
-////GETTING STUCK HERE
-        if (!map[category]) 
-            map[category] = [];
-        map[category].push(groupedByTypeA[r]);
-    }
-    return map;
-/////NOT working
-    console.log(category); 
-  }
+//   groupedByTypeA = Object.keys(groupedByType);
+//   console.log(groupedByTypeA);
+//     for (var r = 0; r<groupedByTypeA.length; r++) {
+//         var category = groupedByTypeA[r];
+//         // console.log(category);  ////each of the 105 objects separate
+// ////GETTING STUCK HERE
+//         if (!p.map[category]) 
+//             p.map[category] = [];
+//         p.map[category].push(groupedByTypeA[r]);
+//     }
+//     return p.map;
+
+//     console.log(category); 
+//   }
 
 
-// callback for loaded data.
-function handleDataLoad(d) {
-  groupedByType = d;
-  redraw();
-}   
+// // callback for loaded data.
+//  p.handleDataLoad(d) = function {
+//   groupedByType = d;
+//   redraw();
+// }   
 
 
-function barChart(){
+p.barChart = function(){
+p.push();
 
 var groupedByType = collate(tableA,22);
 console.log(groupedByType);
 
 var groupedByTypeC = Object.keys(groupedByType);
 console.log(groupedByTypeC);
-    var width = 1200,
-        height = 800,
-        margin = 40
+    var width = p.windowWidth, // canvas width and height
+        height = p.windowHeight,
+        margin = 10,
+        w = p.width - 2 * margin, // chart area width and height
+        h = p.height - 2 * margin;
+  
+  var barWidth =  (h / groupedByTypeC.length) * 0.7; // width of bar
+  var barMargin = (h / groupedByTypeC.length) * 0.01; // margin between two bars
 
 for (var b=0; b< groupedByTypeC.length; b++) {
-
     //var total = groupedByType[groupedByTypeC[b]].length;
     var totalsFiltered = 0;
-    // console.log(total);
     var femtotals = 0;
     var maletotals = 0;
 
@@ -185,12 +185,11 @@ for (var b=0; b< groupedByTypeC.length; b++) {
          maletotals = maletotals + 1;
          //fill(92,242,145,120);
          //rect(20 + (b * 2), 20 + (t * 2), 10, 1);
-      }else if (currentGender == 'f'){
-        femtotals = femtotals + 1;
-       // fill(179,118,244,120);
-        //rect(20 + (b * 2), 20 + (t * 2), 10, 1);
+      } else if (currentGender == 'f'){
+          femtotals = femtotals + 1;
+          // fill(179,118,244,120);
+          //rect(20 + (b * 2), 20 + (t * 2), 10, 1);
       };
-
 
       if(currentGender === 'm' || currentGender === 'f'){
         totalsFiltered = totalsFiltered + 1;
@@ -202,6 +201,11 @@ for (var b=0; b< groupedByTypeC.length; b++) {
    barTotals.push(myObject)
 
       }
+      p.push();
+      p.scale(0.4); 
+      p.rotate(p.radians(90));   // rotate to vertical
+      p.translate(1200, t*(barWidth*300 + barMargin)); // jump to the top right corner
+      // p.translate(150, -5200); // jump to the top right corner of the bar 
 ////sort
       // console.log(barTotals)
       barTotals.sort(function(a,b){
@@ -209,17 +213,46 @@ for (var b=0; b< groupedByTypeC.length; b++) {
       })
       // console.log(barTotals)
       for (var i = barTotals.length - 1; i >= 0; i--) {
-
-         fill(92,242,145,120);
-         rect(100 + (i * 10), 100, 10, barTotals[i]['m']);
-
-         fill(179,118,244,120);
-         rect(100 + (i * 10), 100, 10, (-1) * barTotals[i]['f']);
+          p.noStroke();
+          p.fill(92,242,145,120);
+          p.rect(100 + (i * 30), 100, 50, barTotals[i]['m']);
+          // p.rect(100 + (i * 10), 100, 10, barTotals[i]['m']);
+          p.noStroke();
+          p.fill(179,118,244,120);
+          p.rect(100 + (i * 30), 100, 50, (-1) * barTotals[i]['f']);
 
       };
+
+      p.pop();
+    p.pop();
+    }
+
 }
 
-function lineChart(){
+var myp5 = new p5(s, 'c1');
+
+
+var t = function(p) {
+     p.preload = function(){
+   // table = loadTable('assets/ModContGenderfinalgosort.csv','csv','header');  /////secondary sort gender, classification
+   p.table = p.loadTable('assets/ModContGenderfinalgnoysort.csv','csv','header'); /////four sort gender, Artist Alpha Sort, classification, year
+   console.log(p.table);
+  }
+
+  p.setup = function(){
+    canvas = p.createCanvas(1200, 800);
+    canvas.parent("#c2");
+    // p.analyzeData();
+    // p.categorize();
+    p.lineChart();
+    // handleDataLoad();
+    // barChart();
+    p.noLoop();
+  }
+
+ p.lineChart = function(){
+  p.push();
+
   var maxX1;
   var x1,x2,y1,y2;
   
@@ -237,16 +270,16 @@ function lineChart(){
   console.log(maxX);  ////returns 105
   // console.log(maxX2);  ////returns 
   var maxY = 1500000;
-  textSize(10);
+  p.textSize(10);
 
 // iterate through the data
   for(var i = 0; i < maxX; i++) {    
-    stroke(color(179,118,244));
-    strokeWeight(1);
-    noFill();
+    p.stroke(p.color(179,118,244));
+    p.strokeWeight(1);
+    p.noFill();
     // console.log(Object.keys(groupedByType)[i]);
     var individualPiece = maxX1[i];
-    console.log(individualPiece);
+    // console.log(individualPiece);
 
 
 // variables
@@ -264,9 +297,16 @@ function lineChart(){
     // noStroke()
     // fill(0)
     // text(i,x1,height)
+
   }
+
+
+  p.pop();
 }
 
+}
+
+var myp5 = new p5(t, 'c2');
 //////////////////////
 
 ///COLOR works
