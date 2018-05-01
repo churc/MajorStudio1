89a71c8be6fc26a4bb5d9b9ab84eace4c19087e7
Clare <ccinflux@hotmail.com>
2018-05-01 17:09:23 -0400

    Add files via upload

--- Interactive/genderObj/sketchArtists.js
+++ Interactive/genderObj/sketchArtists.js
@@ -1,4 +1,7 @@
-var artists;
+var artists = [];
+var artistInfo = []; 
+var artistObject;
+
 var img0;
 var img1;
 var img2;
@@ -20,46 +23,65 @@ var img17;
 var img18;
 var img19;
 
-var artistObject;
-
-var items = [];
-var uncert;
-var sent = [];
-var d;
-var rSelect;
 
 var KhandFont, fontReady = false;
 
-var margin = 80;
-
-///////faces in the collection
+var margin = 30;
 
 var w = function(p){
 
+p.preload = function(){
+  artists = p.loadJSON("assets/artists.json", artists);
 
-p.preload = function() {
-    artists = p.loadJSON("assets/artists.json");
+    img0 = p.loadImage('assets/Dove.png');
+    img1 = p.loadImage('assets/Picasso.png');
+    img2 = p.loadImage('assets/Matisse.png');
+    img3 = p.loadImage('assets/Hartley.png');
+    img4 = p.loadImage('assets/Scarpa.png');
+    img5 = p.loadImage('assets/Albers.png');
+    img6 = p.loadImage('assets/Klee.png');
+    img7 = p.loadImage('assets/Marsh.png');
+    img8 = p.loadImage('assets/Stirn.png');
+    img9 = p.loadImage('assets/Pollock.png');
+    img10 = p.loadImage('assets/Liebes.png');
+    img11 = p.loadImage('assets/OKeeffe.png');
+    img12 = p.loadImage('assets/Pape.png');
+    img13 = p.loadImage('assets/Stolzl.png');
+    img14 = p.loadImage('assets/Walkowitz.png');
+    img15 = p.loadImage('assets/Weeber.png');
+    img16 = p.loadImage('assets/Willers.png');
+    img17 = p.loadImage('assets/Zeisel.png');
+    img18 = p.loadImage('assets/Nurmesniemi.png');
+    img19 = p.loadImage('assets/Reindl.png');
 }
+
+
+
+console.log(artists)
+///////artists most collected 
+
+
 
 p.setup = function() {
   p.createCanvas(1440,900);
-  p.showArtists();
-  p.mouseClicked('span'); 
+  // showArtists();
+  p.show();
+  p.mouseClicked('span');
+  p.artists(); 
+  p.label();
 }
 
-  p.windowResized = function() {
+p.windowResized = function() {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
 }
 
 p.show = function() {
-    // this.x = x;
-    // this.y = y;
-
     $('span').click(function() {
-    $('p#title').addClass('titleW');
-    $('p#date').addClass('titleW');
-    $('p#credit').addClass('titleW'); 
-    $('p#desc').addClass('titleW'); //note hide weblabel text 
+    $('#text').addClass('titleA');
+    $('#number').addClass('titleA');
+    $('#medium').addClass('titleA'); 
+    $('#nationality').addClass('titleA'); 
+    $('#dates').addClass('titleA'); 
   })
 }
 
@@ -67,8 +89,72 @@ p.mouseClicked = function(){
   p.show = (!p.show);
 }
   
+p.artists = function(){
+  p.scale(.5);
+  p.image(img0, 500,980);
+  p.textFont('Khand');
+  p.textStyle(p.NORMAL);
+  p.noStroke();
+  p.textAlign(p.LEFT);
+  p.fill(112,112,112);
+  p.textSize(14);
+  p.text("Arthur Dove: 81 artworks", 500,980);
+  p.image(img1, 199,992);
+  p.text("Pablo Picasso: 168 artworks", 199,992);
+  p.image(img5, 990,820);
+  p.text("Anni Albers: 80 artworks", 990,820);
+  p.image(img17, 1300,1250);
+  p.text("Eva Zeisel: 51 artworks", 1300,1250);
+  p.scale(.8);
+  p.image(img6, 556,890);
+  p.text("Paul Klee: 98 artworks", 556,890);
+  p.image(img7, 2010,440);
+  p.text("Reginald Marsh: 927 artworks", 2010,440);
+  p.image(img8, 1903,1137);
+  p.text("Louis A. Stirn: 172 artworks", 1903,1137);
+  p.image(img9, 1011,555);
+  p.text("Jackson Pollock: 117 artworks", 1011,555);
+  p.scale(.9);
+  p.image(img10, 1528,370);
+  p.text("Dorothy Liebes: 89 artworks", 1528,370);
+  p.image(img11, 247,662);
+  p.text("Georgia O’Keeffe: 22 artworks", 247,662);
+  p.image(img12, 3100,1740);
+  p.text("Lygia Pape: 32 artworks", 3100,1740);
+  p.image(img13, 2020,1285);
+  p.text("Gunta Stolzl: 39 artworks", 2020,1285);
+  p.image(img14, 2751,1151);
+  p.text("Abraham Walkowitz: 167 artworks", 2751,1151);
+  p.image(img2, 2003,777);
+  p.text("Henri Matisse: 105 artworks", 2003,777);
+  p.image(img3, 820,554);
+  p.text("Marsden Hartley: 10 artworks", 820,554);
+  p.image(img4, 3480,1600);
+  p.text("Carlo Scarpa: 254 artworks", 3480,1600);
+
+  p.image(img15, 469,210);
+  p.text("Marion Weeber: 35 artworks", 469,210);
+  p.image(img16, 1045,240);
+  p.text("Margarete Willers: 65 artworks", 1045,240);
+  p.scale(1.1); 
+  p.image(img18, 2230,370);
+  p.text("Vuokko Eskolin-Nurmesniemi: 52 artworks", 2230,370);
+  p.image(img19, 2700,480);
+  p.text("Hilde Reindl: 48 artworks", 2700,480);
+
+  }
+
+  p.label = function(){
+  p.textFont('Khand');
+  p.textStyle(p.NORMAL);
+  p.noStroke();
+  p.textAlign(p.LEFT);
+  p.fill(102,102,102);
+  p.textSize(85);
+  p.text("Who are the most collected female and male artists?", 1600, 75);
+  }
 /////who the artists are
-p.showArtists = function () {
+p.showArtists = function() {
   // p.imageMODE(p.CENTER);
 
   // p.scale(0.6);
@@ -97,29 +183,105 @@ p.showArtists = function () {
           artistLink.appendChild(artistImage);
 
           artistObject.appendChild(artistLink);
+        //   canvas.appendChild(artistObject);
+        // // console.log('artist link', artistLink);
+        //   console.log('artist object', artistObject);
+
+
+  ////name, artwork number, medium, nationality, date
+  /////name
+          var name = document.createElement('p');
+
+          var info = artists[n];
+          var lengthInfo = Object.keys(info).length;
+
+          var nameInfo = artists[n].name;
+          name.innerText += nameInfo;
+          name.addClass="text";
+          name.id = "text";
+          artistObject.appendChild(name);
+
+//////artwork number
+          var number = document.createElement('p');
+
+          var infoNumber = artists[n];
+          var lengthNumber = Object.keys(infoNumber).length;
+
+          var total = "Total number of artworks: " + artists[n].artworks;
+          number.innerText += total;
+          number.addClass="number";
+          number.id = "number";
+          artistObject.appendChild(number);
+
+//////artwork medium
+          var medium = document.createElement('p');
+
+          var infoMedium = artists[n];
+          var lengthMedium = Object.keys(infoMedium).length;
+
+          // var media = artists[n].medium.text;
+          var media = artists[n].medium;
+          medium.innerText += media;
+          medium.addClass="medium";
+          medium.id = "medium";
+          artistObject.appendChild(medium);
+
+ ////// nationality        
+          var nationality = document.createElement('p');
+
+          var infoBorn = artists[n];
+          var lengthBorn = Object.keys(infoBorn).length;
+
+          // var born = artists[n].nationality.text;
+          var born = artists[n].nationality;
+          nationality.innerText += born;
+          nationality.addClass="nationality";
+          nationality.id = "nationality";
+          artistObject.appendChild(nationality);
+
+//////dates
+          
+          var dates = document.createElement('p');
+
+          var infoDates = artists[n];
+          var lengthDates = Object.keys(infoDates).length;
+
+          // var born = artists[n].dates.text;
+          var date = artists[n].dates;
+          dates.innerText += date;
+          dates.addClass="dates";
+          dates.id = "dates";
+          artistObject.appendChild(dates);
+
+////attach to canvas
           canvas.appendChild(artistObject);
         // console.log('artist link', artistLink);
           console.log('artist object', artistObject);
+
+          artistInfo.push(artistObject);
+        
+
 
       }
 
   }
 }
-var myp5 = new p5(w, 'canvas-artists');
+var myp5 = new p5( w, 'canvas-artists');
 
 
 //////artists and kind of objects
+
 var whoWhat;
 
 var b = function(p){
 
 
 p.preload = function() {
-  whoWhat = p.loadImage("assets/quant7_forweb_churchouse1-10-06.png");
+  whoWhat = p.loadImage("assets/quant9_forwebsvg_churchouse-01.png");
 }
 
 p.setup = function() {
-  p.createCanvas(1440,3400);
+  p.createCanvas(1440,4200);
   p.showWhoWhat();
 }
 
@@ -130,209 +292,23 @@ p.draw = function() {
 
 ////display png
 p.showWhoWhat = function(){
-  p.scale(0.55);
-  p.image(whoWhat, 250, 120, 2467,6358.5);  
+  p.scale(0.5);
+  p.image(whoWhat, 250, 120, 5064/2,15618/2);  
 
   // p.image(whoWhat, p.mouseX, 0);
 
   }
 }
+
 var myp5 = new p5(b, 'canvas-artworks');
 
-
-  // p.image(img1, 199,992);
-  // p.image(img5, 1270,1820);
-  // p.image(img17, 1300,1100);
-  // p.scale(.7);
-  // p.image(img6, 556,890);
-  // p.image(img7, 2010,440);
-  // p.image(img8, 1903,1137);
-  // p.image(img9, 1011,555);
-  // p.scale(.8);
-  // p.image(img10, 1528,370);
-  // p.image(img11, 247,662);
-  // p.image(img12, 3100,1740);
-  // p.image(img13, 2020,1285);
-  // p.image(img14, 2751,1151);
-  // p.image(img2, 2003,777);
-  // p.image(img3, 820,554);
-  // p.image(img4, 3480,1600);
-  // p.image(img15, 469,210);
-  // p.image(img16, 1045,240);
-  // p.scale(1.3); 
-  // p.image(img18, 2230,370);
-  // p.image(img19, 2700,480);
- 
-
-
-
-//   var length = Object.keys(items).length-1;
-//   console.log(length);
-
-//     for (var n = 0; n < length; n++) {
-      
-//       var met_object = document.createElement('div');
-//       met_object.id = "met_object"+n;
-
-//       $(met_object).addClass("designed-image");
-//       // console.log(met_object);
-// ////image
-//       if (!items[n].message) {
-//           var unc = document.createElement('img');
-//           unc.src = items[n].media.images.primaryImage.webImageUrl;
-//           unc.addClass="designed-image";
-//           unc.id='unc';
-//           // unc.id='unc'+n;
-        
-          // var obj_url = document.createElement('a');
-          // obj_url.target = "blank";
-          // obj_url.href = "https://www.metmuseum.org" + items[n].media.images.primaryImage.objectUrl;
-          // obj_url.appendChild(unc);
-
-//           met_object.appendChild(obj_url);
-//       };
-
-////title
-      // if (!items[n].message && items[n].titles.primaryTitle.includes("uncertain")) {
-      //     var title = document.createElement('p');
-
-      //     var selTitle = items[n];
-      //     var lengthSel = Object.keys(selTitle).length-1; 
-      //     // console.log(lengthSel);
-      //     var text = "Title: " + selTitle.titles.primaryTitle;
-      //     title.innerText += text;
-      //     // title.innerText += more;
-      //     title.addClass="uncertain-title";
-      //     title.id = 'title';
-      //     // title.id = 'title'+n;
-      //     met_object.appendChild(title);
-      //     // console.log(title);
-      // };
-
-// ////tombstone date
-//       if (!items[n].message && items[n].tombstone["1"].text.includes("uncertain")) {
-//           var date = document.createElement('p');
-  
-//           var selDate = items[n];
-//           var lengthDate = Object.keys(selDate).length-1; 
-//           // console.log(lengthDate);
-          
-//           var dateL = "Date: " + selDate.tombstone["1"].text;
-//           date.innerText += dateL;
-//           date.addClass="uncertain-date";
-//           date.id = 'date';
-//           // date.id = 'date'+n;
-//           met_object.appendChild(date);
-//           // console.log(date);
-//       };
-
-// ////tombstone date is in this array for some objects
-//       if (!items[n].message && items[n].tombstone["0"].text.includes("uncertain")) {
-//           var date = document.createElement('p');
-          
-//           var selDate = items[n];
-//           var lengthDate = Object.keys(selDate).length-1; 
-//           // console.log(lengthDate);
-          
-//           var dateL = "Date: " + selDate.tombstone["0"].text;
-//           date.innerText += dateL;
-//           date.addClass="uncertain-date";
-//           date.id = 'date';
-//           // date.id = 'date'+n;
-//           met_object.appendChild(date);
-//           // console.log(date);
-//       };
-
-// ////credit
-//       if (!items[n].message && items[n].tombstone<=["6"]  && items[n].tombstone["6"].label.includes("Uncertain")) {
-//           var credit = document.createElement('p');
-
-//           var selCredit = items[n];
-//           var lengthCredit = Object.keys(selCredit).length-1; 
-//           // console.log(lengthCredit);
-          
-//           var creditL = "Credit: " + selCredit.tombstone["6"].label;
-//           credit.innerText += creditL;
-//           credit.addClass="uncertain-credit";
-//           credit.id = 'credit';
-//           // credit.id = 'credit'+n;
-
-//           met_object.appendChild(credit);
-//           console.log(credit);
-//       };
-
-
-////description label keep hidden - direct to Met website for web label text
-      // if(!items[n].message && items[n].webLabel.text.includes("uncertain")){
-      //     var desc = document.createElement('p');
-
-      //     var selDesc = items[n];
-      //     // var lengthSelD = Object.keys(selDesc).length-1;
-      //     var textD = selDesc.webLabel.text;
-
-      //       if(!items[n].message){
-      //           var de = textD;
-      //       ////split text into separate sentences
-      //         var sent = de.split('.');
-      //         // console.log(sent);
-      //       ///////go through sentences and return each of those with uncertain
-      //         for (var j=0; j<sent.length; j++){
-      //           if (sent[j].includes("uncertain"||"Uncertain")){
-      //             var d = sent[j];
-      //             d = d.replace(/[<]br[^>]*[>]/gi,"");
-      //            }      
-      //         }
-      //           desc.innerText += d;
-      //           desc.addClass = "uncertain-des";
-      //           desc.id = 'des';
-      //           // desc.id = 'des'+n;
-      //           met_object.appendChild(desc);
-
-      //       };
-      //     }
-      //     document.body.appendChild(met_object);
-          
-//           MetArray.push(met_object);
-
-//     };
-//           // console.log(MetArray);
-
-// ////span to isolate word uncertain - for styling  
-//       $('p').each(function(){
-//         var uncert = $(this).text().replace(/uncertain/g,"<span> uncertain </span>");
-//         $(this).html(uncert);
-//         // console.log(uncert);
-//       });
-
-
-
-// var rSelect = document.createElement("button");
-
-// function shuffleMyArray(){
-//     var button = document.getElementById("rSelect");
-
-//     $('.designed-image').remove()
-//     shuffle(MetArray,true);
-//     for (var r=0; r<MetArray.length; r++){
-//       document.body.appendChild(MetArray[r]);
-//     };
-
-//     $('p').each(function(){
-//         var uncert = $(this).text().replace(/uncertain/g,"<span>uncertain</span>");
-//         $(this).html(uncert);
-//       });
-
-// button.onclick = function() {
-//     shuffleMyArray();
-//     };
-//   };
 
 
 
 
 // $("div#c2").append('<a href="' + "https://churc.github.io/MajorStudio1/MetProjects/gender" + "c2" + '</a>');
 
-///////////load svg
+
 
 
 
