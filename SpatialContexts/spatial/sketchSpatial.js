122667c3a52cfe045b381a1f43a86388e9e5bfa5
Clare <ccinflux@hotmail.com>
2018-05-12 15:44:32 -0400

    Add files via upload

--- SpatialContexts/spatial/sketchSpatial.js
+++ SpatialContexts/spatial/sketchSpatial.js
@@ -9,19 +9,19 @@ var margin = 30;
 
 
 
-//////========artists and kind of objects===========
+//////========spatial intro exhibition entrance===========
 
 // var whoWhat;
 
 var b = function(p){
 
 p.preload = function() {
-  spatial1 = p.loadImage("assets/fashionMoMA3_crop-01.png");
+  spatial1 = p.loadImage("assets/museum_extRev-01.png");
   KhandFont = p.loadFont('libraries/Khand-Regular.ttf', p.fontRead);
 }
 
 p.setup = function() {
-  p.createCanvas(1440,800);
+  p.createCanvas(1440, 940);
   p.showSpatial1();
   p.noLoop();
 }
@@ -29,22 +29,17 @@ p.setup = function() {
 ////display png
 p.showSpatial1 = function(){
   // p.scale(0.95);
-  p.image(spatial1, 0, 225, 1015, 615);
+  p.image(spatial1, 0, 225, 1004, 605);
 
   }
-console.log("working2") 
-// ////display png
-// p.showWhoWhat = function(){
-//   p.scale(0.5);
-//   p.translate(-20,325);
-//   p.image(whoWhat, 250, -80, 2430,7962);  
-//   }
+console.log("working1") 
+
 }
 
 var myp5 = new p5(b, "canvas-spatial1");
 
 
-//////======spatial intro exhibition========
+//////======spatial exhibition========
 
 
 
@@ -55,27 +50,62 @@ p.fontRead = function(){
     }
 
 p.preload = function() {
-  spatial2 = p.loadImage("assets/emptyspace2-01.png");
+  spatial2 = p.loadImage("assets/museum_intRev2-01.png");
   KhandFont = p.loadFont('libraries/Khand-Regular.ttf', p.fontRead);
 }
 
 p.setup = function() {
-  p.createCanvas(1440,650);
+  p.createCanvas(1440,640);
   p.showSpatial2();
   p.noLoop();
 }
 
 ////display png
 p.showSpatial2 = function(){
-  p.scale(0.55);
+  p.scale(0.328);
 
-  p.image(spatial2, 0, 95, 1960, 1103);  
+  p.image(spatial2, 0, 95, 3060, 1418);  
   
   }
-
+console.log("working2") 
 }
 var myp5 = new p5(l, "canvas-spatial2");
 
+
+////=========ar=========
+
+
+//////======spatial intro exhibition========
+
+
+
+var c = function(p){
+
+p.fontRead = function(){
+      fontReady = true; 
+    }
+
+p.preload = function() {
+  spatial3 = p.loadImage("assets/museum_intRev2-mobile-blur-24-01.png");
+  KhandFont = p.loadFont('libraries/Khand-Regular.ttf', p.fontRead);
+}
+
+p.setup = function() {
+  p.createCanvas(1440,850);
+  p.showSpatial2();
+  p.noLoop();
+}
+
+////display png
+p.showSpatial2 = function(){
+  p.scale(0.079);
+
+  p.image(spatial3, 0, 95, 12773, 5869);  
+  
+  }
+console.log("working3") 
+}
+var myp5 = new p5(c, "canvas-spatial3");
 
 
 
