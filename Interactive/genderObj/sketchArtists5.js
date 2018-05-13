97d4f1108eb54daa8ac7d3ec563ef4839d6595bf
Clare <ccinflux@hotmail.com>
2018-05-12 21:15:48 -0400

    Update sketchArtists5.js

--- Interactive/genderObj/sketchArtists5.js
+++ Interactive/genderObj/sketchArtists5.js
@@ -134,7 +134,7 @@ function switchText(indivType, x, y){
     // $('#tooltip').css('left', x); 
   ////1440 screen
     $('#tooltip').css('top', y-640 + $('#canvas-sketchBar-vertical').position().y); ////position of canvas
-    $('#tooltip').css('left', x-140);
+    $('#tooltip').css('left', x-240);
   ////1980 screen
     // $('#tooltip').css('top', y-630 + $('#canvas-sketchBar-vertical').position().y); ////position of canvas
    // $('#tooltip').css('left', x-70);
@@ -347,14 +347,14 @@ barTotalsSort = barTotals;
           p.stroke("#ffffff");
           p.fill(92,242,145,150); ////green bars artworks by men
       ////1440 screen
-          p.rect(160 + (i * 10), $(window).height()/2, 10, barTotals[i]['m']/2);
+          p.rect(160 + (i * 10), $(window).height()/1.9, 10, barTotals[i]['m']/2);
       ////1980 screen
           // p.rect(160 + (i * 10), $(window).height()/2.87, 10, barTotals[i]['m']/2);
 
           p.fill(179,118,244,140);  ////purple bars artworks by women
       ////1440 screen
-          var bar = p.rect(160 + (i * 10), $(window).height()/2, 10, (-1) * barTotals[i]['f']/2);
-          bars.push({x:160 + (i * 10), y:$(window).height()/2, width: 10, height: barTotals[i]['f']/2, name:barTotals[i].name, maleHeight: barTotals[i]['m']/2});
+          var bar = p.rect(160 + (i * 10), $(window).height()/1.9, 10, (-1) * barTotals[i]['f']/2);
+          bars.push({x:160 + (i * 10), y:$(window).height()/1.9, width: 10, height: barTotals[i]['f']/2, name:barTotals[i].name, maleHeight: barTotals[i]['m']/2});
 
       ////1980 screen
           // var bar = p.rect(160 + (i * 10), $(window).height()/2.87, 10, (-1) * barTotals[i]['f']/2);
